28fd1347b2b6135d0de1e2fa193a55bc9371b558
Khrystyna <pankiv.khrystyna07@gmail.com>
2026-04-24 00:28:57 +0300

    add array

--- main.cpp
+++ main.cpp
@@ -2,22 +2,30 @@
 #include <vector>
 #include <deque>
 #include <array>
-#include <string>
+#include "Product.h"
 
-class Product {
-public:
-    std::string name;
-    int code;
-    Product(std::string n = "", int c = 0) : name(n), code(c) {}
-    bool operator<(const Product& other) const { return code < other.code; }
-    bool operator==(const Product& other) const { return code == other.code; }
-    bool is_even() const { return code % 2 == 0; }
-};
-
-std::ostream& operator<<(std::ostream& os, const Product& p) {
-    return os << "[" << p.name << ":" << p.code << "]";
+template <typename T, std::size_t A, std::size_t B>
+std::array<T, A + B> merge_arrays(const std::array<T, A>& a, const std::array<T, B>& b) {
+    std::array<T, A + B> result;
+    auto it1 = a.begin();
+    auto it2 = b.begin();
+    auto res_it = result.begin();
+    while (it1 != a.end() && it2 != b.end()) {
+        if (*it1 < *it2) *res_it++ = *it1++;
+        else *res_it++ = *it2++;
+    }
+    while (it1 != a.end()) *res_it++ = *it1++;
+    while (it2 != b.end()) *res_it++ = *it2++;
+    return result;
 }
 
+
 int main() {
-    return 0;
+    std::array<int, 3> a1 = {1, 5, 9};
+    std::array<int, 2> a2 = {2, 4};
+    auto res = merge_arrays(a1, a2);
+    std::cout << "Merged Array: ";
+    for(int x : res) std::cout << x << " ";
+
+      return 0;
 }
