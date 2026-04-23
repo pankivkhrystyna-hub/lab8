5e8d5e2f325f4099229c84c7d7f47f17fdcecb49
Khrystyna <pankiv.khrystyna07@gmail.com>
2026-04-24 00:31:39 +0300

    Розбиття: розділіть std::vector на дві групи

--- main.cpp
+++ main.cpp
@@ -19,7 +19,17 @@ std::array<T, A + B> merge_arrays(const std::array<T, A>& a, const std::array<T,
     return result;
 }
 
+template <typename T>
+void split_by_parity(const std::vector<T>& src, std::vector<T>& even, std::vector<T>& odd) {
+    for (auto it = src.begin(); it != src.end(); ++it) {
+        bool is_even_val;
+        if constexpr (std::is_integral_v<T>) is_even_val = (*it % 2 == 0);
+        else is_even_val = it->is_even();
 
+        if (is_even_val) even.push_back(*it);
+        else odd.push_back(*it);
+    }
+}
 int main() {
     std::array<int, 3> a1 = {1, 5, 9};
     std::array<int, 2> a2 = {2, 4};
@@ -27,5 +37,12 @@ int main() {
     std::cout << "Merged Array: ";
     for(int x : res) std::cout << x << " ";
 
-      return 0;
+
+    std::vector<Product> shop = { {"Apple", 10}, {"Bread", 5}, {"Milk", 12} };
+    std::vector<Product> even_p, odd_p;
+    split_by_parity(shop, even_p, odd_p);
+    std::cout << "\nEven Products: " << even_p.size();
+
+    
+    return 0;
 }
